2b9d6e46524aa5e04939c491b1a5be3b2ea14dcb
motosasha <moto.sasha@gmail.com>
2023-05-14 22:33:12 +0300

    add gh-pages

--- src/js/common/simpleBar.js
+++
@@ -1,9 +0,0 @@
-import ready from "../utils/documentReady.js";
-import SimpleBar from "simplebar";
-
-ready(function () {
-  Array.prototype.forEach.call(
-    document.querySelectorAll(".panel__scroll"),
-    (el) => new SimpleBar(el, { autoHide: false }),
-  );
-});
